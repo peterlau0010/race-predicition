58e82d81811a06f771da6521ff5258a99c5bf3a2
Peter Lau <peterl@hk1.ibm.com>
2019-04-16 12:36:38 +0800

    Update WebCrawling.py

--- WebCrawling.py
+++ WebCrawling.py
@@ -26,7 +26,7 @@ class WebCrawling:
 
     def reFormatText(self, text):
         # Trim space, remove line break, remove double space
-        return re.sub(' +', ' ', text.replace('\n', ' ').replace('\r', '').strip())
+        return re.sub(' +', ' ', text.replace('\n', ' ').replace('\r', '').replace(',', '').strip())
 
     def getHistory(self, day, racecource, raceno):
         url_base = 'https://racing.hkjc.com/racing/information/English/Racing/LocalResults.aspx?'
@@ -41,23 +41,52 @@ class WebCrawling:
 
         table = html_source.find(
             name='table', class_='f_tac table_bd draggable')
-        history = []
 
-        for t in table.find_all(name='td'):
-            history.append(self.reFormatText(t.text))
+        matchDetail = html_source.find(name='tbody', class_='f_fs13')
+
+        # Initital history list
+        history = []
+        history_header = []
+
+        # Add Index value to history
+        history_header.append(day)
+        history_header.append(racecource)
+        history_header.append(raceno)
+
+        # Add Match Detail to history
+        for t in matchDetail.find_all(name='td'):
+            # logging.debug( self.reFormatText(t.text))
+            history_header.append(self.reFormatText(t.text))
+
+        # Reformat Match Detail
+        history_header = history_header[0:3] + history_header[6:13]
+        history_header.append(history_header[3].split(' - ')[0])
+        history_header.append(history_header[3].split(' - ')[1])
+        del history_header[3]
+        del history_header[3]
+        del history_header[5]
+        # logging.info(history_header)
+
+        for tr in table.find_all(name='tr'):
+            history += history_header
+            for td in tr.find_all(name='td'):
+                history.append(self.reFormatText(td.text))
 
         logging.debug('Before reFormat, Shape of Histroy: %s',
                       numpy.shape(history))
-        history = numpy.reshape(history, (-1, 12))
+
+        history = numpy.reshape(history, (-1, 21))
+
         logging.debug('After reFormat, Shape of Histroy: %s',
                       numpy.shape(history))
+
         logging.debug('Remove Header')
         history = numpy.delete(history, (0), axis=0)
-
+        logging.debug(history)
         return history
 
 
-history = numpy.zeros(shape=(0, 12))
+history = numpy.zeros(shape=(0, 21))
 matchCSV = pandas.read_csv('matchDate.csv', sep=',')
 matchCSV = pandas.DataFrame(matchCSV)
 
@@ -66,7 +95,12 @@ try:
     webCrawling = WebCrawling()
 
     for index, row in matchCSV.iterrows():
+
         for matchIndex in range(row['RaceNo']):
+            # Logic for debug use,
+            if matchIndex > 2:
+                break
+            
             day = '{:0>4d}'.format(row['RaceDate1']) + '/' + '{:0>2d}'.format(
                 row['RaceDate2']) + '/' + '{:0>2d}'.format(row['RaceDate3'])
             racecource = row['Racecourse']
@@ -74,9 +108,12 @@ try:
             # logging.info('day = %s, racecource=%s, raceno = %s ',
             #  day, racecource, raceno)
             temp_history = webCrawling.getHistory(day, racecource, raceno)
+            logging.info(temp_history)
             history = numpy.concatenate((history, temp_history), axis=0)
+        else:
+            continue
+        break
 
-    webCrawling.close()
 
 except Exception as ex:
     logging.error(ex)
