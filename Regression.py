dd87968bc4ce4833ed1557e92c2b3ed16a70ee1f
Peter Lau <peterl@hk1.ibm.com>
2019-04-24 18:27:32 +0800

    update

--- Regression.py
+++ Regression.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
@@ -16,11 +15,30 @@ from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import time
 
+dist = '1200M'
+
 # Read CSV file
 data = pd.read_csv('history_bak.csv', header=0)
+data = data.iloc[::-1]
+print(np.shape(data))
 
 # Remove '---' value
 data = data[data.finishTime != '---']
+data = data[data.awt != '---']
+data = data[data.dhw != '---']
+
+# Group category
+data.loc[ data['class'].str.contains('Class 2'), 'class'] = 'Class 2'
+data.loc[ data['class'].str.contains('Class 3'), 'class'] = 'Class 3'
+data.loc[ data['class'].str.contains('Class 4'), 'class'] = 'Class 4'
+data.loc[ data['road'].str.contains('"A+'), 'road'] = 'TURF - "A" Course'
+data.loc[ data['road'].str.contains('"B+'), 'road'] = 'TURF - "B" Course'
+data.loc[ data['road'].str.contains('"C+'), 'road'] = 'TURF - "C" Course'
+print(np.shape(data))
+
+# Match information
+data = data[ data.dist == dist]
+
 
 # Prepare reformat data
 reformat_data = data.copy()
@@ -30,7 +48,7 @@ reformat_data['finishTime'] = (pd.to_datetime(
     reformat_data['finishTime'], format="%M:%S.%f") - datetime(1900, 1, 1))/timedelta(milliseconds=1)
 
 X_train_original, X_test_original, y_train_original, y_test_original = train_test_split(
-    reformat_data, reformat_data, test_size=0.1, shuffle=False)
+    reformat_data, reformat_data, test_size=0.05, shuffle=False)
 
 # Process Category data
 reformat_data = pd.get_dummies(
@@ -45,20 +63,21 @@ reformat_data = pd.get_dummies(reformat_data, columns=[
                                'draw'], prefix=['draw'])
 reformat_data = pd.get_dummies(
     reformat_data, columns=['class'], prefix=['class'])
-reformat_data = pd.get_dummies(reformat_data, columns=[
-    'money'], prefix=['money'])
+# reformat_data = pd.get_dummies(reformat_data, columns=[
+#     'money'], prefix=['money'])
 reformat_data = pd.get_dummies(reformat_data, columns=[
                                'road'], prefix=['road'])
-reformat_data = pd.get_dummies(reformat_data, columns=[
-                               'dist'], prefix=['dist'])
-
+# reformat_data = pd.get_dummies(reformat_data, columns=[
+#                                'dist'], prefix=['dist'])
+# reformat_data = pd.get_dummies(reformat_data, columns=[
+#                                'awt'], prefix=['awt'])
 
 # Split train and test
 X = reformat_data.drop(['finishTime', 'date', 'raceNo', 'raceName',
-                        'plc', 'horseNo', 'lbw', 'runPos', 'odds', 'horseName'], axis=1)
+                        'plc', 'horseNo', 'lbw', 'runPos', 'odds','horseName','money','dist'], axis=1)
 y = reformat_data['finishTime']
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, shuffle=False)
+    X, y, test_size=0.05, shuffle=False)
 
 print(X_train.columns.values)
 
@@ -80,14 +99,18 @@ print("Mean squared error: %.2f"
 print('Variance score: %.2f' % r2_score(y_test, y_pred))
 
 # Plot outputs
+
 plt.scatter(y_test, y_pred)
+plt.ylim(68000, 73000)
+plt.xlim(68000, 73000)
 plt.show()
 
 X_test_original['p_finishTime'] = y_pred
 
-print(X_test_original.head())
+# print(X_test_original.head())
 
 headers = 'date,raceCource,raceNo,going,raceName,road,money,class,dist,plc,horseNo,horseName,jockey,trainer,awt,dhw,draw,lbw,runPos,finishTime,odds,p_finishTime'
 
-np.savetxt('regression_report.csv', X_test_original,
+np.savetxt('regression_report.csv', X_test_original.round(0),
            delimiter=',', fmt='%s', header=headers)
+
