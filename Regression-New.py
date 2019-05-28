0118790f1d4e79c5f75d1975efaaa83249dac75b
Peter Lau <peterlau0010@gmail.com>
2019-05-28 22:15:54 +0800

    update

--- Regression-New.py
+++ Regression-New.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import mean_squared_error,accuracy_score
 
 # ---------- Parameter
-date = '20190526'
+date = '20190529'
 # raceCourse = 'ST'
 # test_size = 0.01 # For real prediction
 # test_size = 0.07 # For backtest predictionx
@@ -191,13 +191,13 @@ Select requried columns for train, test, predict
 # train_test_col =['class', 'Draw', 'Age', 'AWT', 'Horse Wt. (Declaration)', 'Rtg.+/-', 'Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'raceCourse_ST', 'horseRank', 'DamRank', 'JockeyRank']
 
 # 0.75 / 0.3542 test split 0.2 unit 20190526 data
-# train_test_col =['class', 'Draw', 'Age', 'AWT', 'Horse Wt. (Declaration)', 'Rtg.+/-', 'Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'JockeyRank', 'raceCourse_ST', 'horseRank', 'DamRank']
+train_test_col =['class', 'Draw', 'Age', 'AWT', 'Horse Wt. (Declaration)', 'Rtg.+/-', 'Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'JockeyRank', 'raceCourse_ST', 'horseRank', 'DamRank']
 
 # 0.67 / 0.23 test split 0.2 unit 20190526 data
 # train_test_col =[ 'Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'Runs_5', 'Runs_6', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'SR', 'P', 'PC', 'E', 'BO', 'PS', 'SB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'horseRank', 'JockeyRank', 'raceCourse_ST', 'DamRank','class', 'Draw', 'Age', 'AWT', 'Horse Wt. (Declaration)', 'Rtg.+/-',]
 
 # 0.73 / 0.33 test split 0.2 unit 20190526 data
-train_test_col =['Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'Runs_5', 'Runs_6', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'SR', 'P', 'PC', 'E', 'BO', 'PS', 'SB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'horseRank', 'JockeyRank', 'raceCourse_ST', 'DamRank', 'class', 'AWT', 'Age', 'Rtg.+/-', 'Horse Wt. (Declaration)', 'Draw']
+# train_test_col =['Runs_1', 'Runs_2', 'Runs_3', 'Runs_4', 'Runs_5', 'Runs_6', 'B', 'H', 'TT', 'CP', 'V', 'XB', 'SR', 'P', 'PC', 'E', 'BO', 'PS', 'SB', 'Sex_c', 'Sex_f', 'Sex_g', 'Sex_h', 'Sex_r', 'going_GOOD', 'going_GOOD TO FIRM', 'going_GOOD TO YIELDING', 'going_YIELDING', 'raceCourse_HV', 'TrainerRank', 'SireRank', 'horseRank', 'JockeyRank', 'raceCourse_ST', 'DamRank', 'class', 'AWT', 'Age', 'Rtg.+/-', 'Horse Wt. (Declaration)', 'Draw']
 
 perm = itertools.permutations(train_test_col)
 
@@ -370,24 +370,141 @@ def train(train_test_col):
     # logging.info('Accuracy score for first 3 recent 10 matches: %s ',
     #             accuracy_score(test_report['real_first_3'], test_report['pred_plc']))
 
+X_train = X_train_copy[train_test_col]
 
-# train_test_col =['class', 'Draw', 'Age', 'AWT',]
-# train_test_col =['class', 'Draw', 'Age',]
-perm = itertools.permutations(train_test_col)
-if __name__ == "__main__":
-    # global score_first_1
-    # global score_first_3
-    names = perm
-    procs = []
-    score_first_1 = Value('f', 0)
-    score_first_3 = Value('f', 0)
+X_train = X_train.astype(float)
+# print(train_test_col)
+X_train = X_train[train_test_col]
 
-    # instantiating process with arguments
-    pool = Pool(processes=3)
-    for name in names:
-        pool.map_async(train, (name,))
-    pool.close()
-    pool.join()
+X_train = X_train.astype(float)
+
+# ---- save columns for further test or prediciton
+predictionColumns = X_train.columns.values
+
+# --------- Fill all missing data
+X_train_backup = X_train
+X_train.fillna(X_train.mean(), inplace=True)
+# X_test.fillna(X_train.mean(), inplace=True)
+# logging.info('Test data filled NaN:  %s \n %s' , np.shape(X_test), X_test.head(2).append( X_test.tail(2)))
+
+""" 
+Scale data
+Train model
+"""
+# --------- Scaler data
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+
+# ---------- Regression model
+# model = MLPRegressor(hidden_layer_sizes=(10,10,10,10,10,10,),activation='relu', solver='lbfgs', alpha=0.0001, shuffle=False, random_state=None,learning_rate='constant') # 0.64 Test size 70%
+model = MLPRegressor(hidden_layer_sizes=(5, 5, 5, 5, 5, 5), activation='relu', solver='adam', alpha=0.00001,
+                    shuffle=True, random_state=13, learning_rate='constant', max_iter=3000)  # 0.74 Test size 50%
+# model = MLPRegressor(hidden_layer_sizes=(3,3,3,3),activation='relu', solver='lbfgs', alpha=0.0001, shuffle=True,)
+model.fit(X_train, y_train.values.ravel())
+
+""" 
+Test the model with X_test
+Generate Test result report
+"""
+
+# ---- Set up for X_test
+X_test = pd.merge(X_test_copy, horseRank[['horseRank', 'Brand No.']], how='left',
+                left_on=['Brand No.'], right_on=['Brand No.'])
+X_test = pd.merge(X_test, sireRank, how='left',
+                left_on=['Sire'], right_on=['Sire'])
+X_test = pd.merge(X_test, damRank, how='left',
+                left_on=['Dam'], right_on=['Dam'])
+X_test = pd.merge(X_test, jockeyRank[['JockeyRank', 'Jockey']], how='left',
+                left_on=['Jockey'], right_on=['Jockey'])
+X_test = pd.merge(X_test, trainerRank[['TrainerRank', 'Trainer']], how='left',
+                left_on=['Trainer'], right_on=['Trainer'])
+
+# ---- Fill missing data
+X_test.fillna(X_train_backup.mean(), inplace=True)
+
+# ---- Select required columns
+X_test = X_test[predictionColumns]
+
+# ---- scale data and test will trianed model
+X_test = X_test.astype(float)
+X_test = scaler.transform(X_test)
+y_pred = model.predict(X_test)
+
+# ---- Prepare data for report generation
+y_test.loc[:, 'pred_finishTime'] = y_pred
+test_report = pd.merge(X_copy, y_test, how='right',
+                    left_index=True, right_index=True)
+
+test_report["pred_plc"] = test_report.groupby(
+['date', 'raceNo'])["pred_finishTime"].rank()
+test_report["real_plc"] = test_report.groupby(['date', 'raceNo'])[
+"plc"].rank()
+
+# ---- Generate Report
+test_report = test_report[['date', 'raceNo', 'horseNo',
+                        'plc', 'odds', 'pred_finishTime', 'real_plc', 'pred_plc', ]]
+
+headers = ','.join(map(str, test_report.columns.values))
+np.savetxt('./Report/test_result_'+date+'.csv', test_report.round(0),
+        delimiter=',', fmt='%s', header=headers, comments='')
+
+# ---- Accuracy rate
+test_report = test_report[(test_report['pred_plc'] <= 1) & (
+test_report['odds'].astype(float) <= 20)]
+# test_report = test_report[(test_report['pred_plc'] <= 1) ]
+
+test_report.loc[test_report['real_plc'] <= 3, 'real_first_3'] = 1
+test_report.fillna(0, inplace=True)
+logging.info('test_report: %s \n %s', np.shape(test_report), test_report)
+
+print('Accuracy score for 1st: %s', accuracy_score(test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+print('Accuracy score for first 3: %s ', accuracy_score(test_report['real_first_3'], test_report['pred_plc']))
+# print('score_first_3.value: ',score_first_3.value)
+# print('score_first_1.value: ',score_first_1.value)
+
+logging.info('Accuracy score for 1st: %s', accuracy_score(
+    test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+logging.info('Accuracy score for first 3: %s ', accuracy_score(
+    test_report['real_first_3'], test_report['pred_plc']))
+
+test_report = test_report.tail(20)
+print('Accuracy score for 1st recent 20 matches: %s', accuracy_score(
+    test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+print('Accuracy score for first 3 recent 20 matches: %s ', accuracy_score(
+    test_report['real_first_3'], test_report['pred_plc']))
+print()
+
+logging.info('Accuracy score for 1st recent 20 matches: %s', accuracy_score(
+    test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+logging.info('Accuracy score for first 3 recent 20 matches: %s ',
+            accuracy_score(test_report['real_first_3'], test_report['pred_plc']))
+
+test_report = test_report.tail(10)
+print('Accuracy score for 1st recent 10 matches: %s', accuracy_score(test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+print('Accuracy score for first 3 recent 10 matches: %s ', accuracy_score(test_report['real_first_3'], test_report['pred_plc']))
+print()
+
+logging.info('Accuracy score for 1st recent 10 matches: %s', accuracy_score(
+    test_report['real_plc'].round(0), test_report['pred_plc'].round(0)))
+logging.info('Accuracy score for first 3 recent 10 matches: %s ',
+            accuracy_score(test_report['real_first_3'], test_report['pred_plc']))
+# perm = itertools.permutations(train_test_col)
+# if __name__ == "__main__":
+#     # global score_first_1
+#     # global score_first_3
+#     names = perm
+#     procs = []
+#     score_first_1 = Value('f', 0)
+#     score_first_3 = Value('f', 0)
+
+#     # instantiating process with arguments
+#     pool = Pool(processes=3)
+#     # for name in names:
+#     #     pool.map_async(train, (name,))
+#     pool.map_async(train, (train_test_col,))
+#     pool.close()
+#     pool.join()
 
     # complete the processes
     # for proc in procs:
@@ -399,7 +516,7 @@ if __name__ == "__main__":
 # print(col)
 
 
-exit()
+# exit()
 
 
 
@@ -444,6 +561,7 @@ pred_data = pd.merge(pred_data, split_result, how='right',
 split_result = pred_data["Gear"].str.split("/", expand=True)
 split_result = split_result.replace(
     to_replace='.-|1|2', value=np.nan, regex=True)
+# split_result.columns = ['Gear_1', 'Gear_2', 'Gear_3', 'Gear_4']
 split_result.columns = ['Gear_1', 'Gear_2', 'Gear_3', 'Gear_4']
 pred_data = pd.merge(pred_data, split_result, how='right',
                 left_index=True, right_index=True)
